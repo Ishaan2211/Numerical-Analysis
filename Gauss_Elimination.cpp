5ac570127fe14d118d033930397e2350ef3e0cd3
Ishaan2211 <krishrawat145@gmail.com>
2025-05-02 22:38:14 +0530

    Add files via upload

---
+++ Gauss_Elimination.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <stdlib.h>
+#define SIZE 10
+
+using namespace std;
+
+int main() 
+{
+    float a[SIZE][SIZE + 1], x[SIZE], ratio;
+    int i, j, k, n;
+
+    cout << "Enter the number of unknowns: ";
+    cin >> n;
+
+    for (i = 1; i <= n; i++) 
+    {
+        for (j = 1; j <= n + 1; j++) 
+        {
+            cout << "a[" << i << "][" << j << "] = ";
+            cin >> a[i][j];
+        }
+    }
+
+    for (i = 1; i <= n - 1; i++) 
+    {
+        if (a[i][i] == 0.0) 
+        {
+            cout << "Mathematical Error! (Zero pivot element)\n";
+            exit(0);
+        }
+
+        for (j = i + 1; j <= n; j++) 
+        {
+            ratio = a[j][i] / a[i][i];
+            for (k = 1; k <= n + 1; k++) 
+            {
+                a[j][k] = a[j][k] - ratio * a[i][k];
+            }
+        }
+    }
+
+    x[n] = a[n][n + 1] / a[n][n];
+
+    for (i = n - 1; i >= 1; i--) 
+    {
+        x[i] = a[i][n + 1];
+        for (j = i + 1; j <= n; j++) 
+        {
+            x[i] = x[i] - a[i][j] * x[j];
+        }
+        x[i] = x[i] / a[i][i];
+    }
+
+    cout << "\nSolution:\n";
+    for (i = 1; i <= n; i++) 
+    {
+        cout << "x[" << i << "] = " << x[i] << endl;
+    }
+
+    return 0;
+}
